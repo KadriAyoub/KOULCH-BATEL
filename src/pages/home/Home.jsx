a1a02a1dc20f288da5d615901b44187210d848a5
berchiche willem <berwillempro@gmail.com>
2026-08-19 11:20:19 +0100

    basic config

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,53 +1,45 @@
-import NavBar from "../../components/common/navBar/NavBar"
-import selecto from '../../assets/selecto.png'
-import 'boxicons'
-import './Home.css'
-import ProductCard from "../../components/product/ProductCard"
-import t_shirt from '../../assets/t-shirt.png'
-import PageNumber from "../components/PageNumber/PageNumber"
-import Footer from "../../components/common/footer/Footer"
-
-
+import "boxicons";
+import selecto from "../../assets/images/selecto.png";
+import t_shirt from "../../assets/images/t-shirt.png";
+import PageNumber from "../../components/PageNumber/PageNumber";
+import ProductCard from "../../components/product/ProductCard";
+import "./Home.css";
 export default function Home() {
-
-
-    return (
-        <div className="home-page">
-            <NavBar />
-            <div className="ads-container">
-                <img className="ads" src={selecto} alt="selecto" />
-            </div>
-            <div className="search-bar">
-                <input type="text" placeholder="Search by name, categorie ..." />
-                <button>
-                    <box-icon className='icon' name='filter-alt' color='white'></box-icon>
-                </button>
-            </div>
-            <div className="product-container">
-                <ProductCard image={t_shirt} />
-                <ProductCard image={t_shirt} />
-                <ProductCard image={t_shirt} />
-                <ProductCard image={t_shirt} />
-                <ProductCard image={t_shirt} />
-                <ProductCard image={t_shirt} />
-                <ProductCard image={t_shirt} />
-                <ProductCard image={t_shirt} />
-                <ProductCard image={t_shirt} />
-                <ProductCard image={t_shirt} />
-                <ProductCard image={t_shirt} />
-                <ProductCard image={t_shirt} />
-            </div>
-            <div className="page-numbers">
-                <PageNumber number={1} />
-                <PageNumber number={2} />
-                <PageNumber number={3} />
-                <PageNumber number={4} />
-                <PageNumber number={5} />
-                <div className="next-page">
-                    <box-icon name='chevrons-right' color='#00229C'></box-icon>
-                </div>
-            </div>
-            <Footer/>
+  return (
+    <div className="home-page">
+      <div className="ads-container">
+        <img className="ads" src={selecto} alt="selecto" />
+      </div>
+      <div className="search-bar">
+        <input type="text" placeholder="Search by name, categorie ..." />
+        <button>
+          <box-icon className="icon" name="filter-alt" color="white"></box-icon>
+        </button>
+      </div>
+      <div className="product-container">
+        <ProductCard image={t_shirt} />
+        <ProductCard image={t_shirt} />
+        <ProductCard image={t_shirt} />
+        <ProductCard image={t_shirt} />
+        <ProductCard image={t_shirt} />
+        <ProductCard image={t_shirt} />
+        <ProductCard image={t_shirt} />
+        <ProductCard image={t_shirt} />
+        <ProductCard image={t_shirt} />
+        <ProductCard image={t_shirt} />
+        <ProductCard image={t_shirt} />
+        <ProductCard image={t_shirt} />
+      </div>
+      <div className="page-numbers">
+        <PageNumber number={1} />
+        <PageNumber number={2} />
+        <PageNumber number={3} />
+        <PageNumber number={4} />
+        <PageNumber number={5} />
+        <div className="next-page">
+          <box-icon name="chevrons-right" color="#00229C"></box-icon>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
